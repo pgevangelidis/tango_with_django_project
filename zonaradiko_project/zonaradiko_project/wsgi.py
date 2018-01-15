06f0d758f5cf41c3c9ff45213981b8de450eabd9
Pavlos Evangelidis <2349102E@student.gla.ac.uk>
2018-01-15 22:26:17 +0000

    ITech tango_with_django_project until chapter 4

--- zonaradiko_project/zonaradiko_project/wsgi.py
+++ zonaradiko_project/zonaradiko_project/wsgi.py
@@ -1,16 +1,14 @@
 """
-WSGI config for pyrihios project.
+WSGI config for zonaradiko_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zonaradiko_project.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyrihios.settings")
-
 application = get_wsgi_application()
